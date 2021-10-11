f056453d19579ea3fca779d1b4859d0542190bc6
RYU <huyaye@naver.com>
2021-10-11 14:08:14 +0900

    Initialize Maven project
    Signed-off-by: RYU <huyaye@naver.com>

---
+++ src/main/java/com/jw/home/JwHomeApiServerApplication.java
@@ -0,0 +1,13 @@
+package com.jw.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwHomeApiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwHomeApiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jw/home/JwHomeApiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jw.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwHomeApiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
